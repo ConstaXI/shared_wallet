fac7a84ac81993ec31f7468e431fee774b3922c5
Davi Banfi <davibanfi1579@outlook.com>
2022-04-14 09:23:23 -0300

    feat: add method to reduce allowance

--- contracts/simpleWallet.sol
+++ contracts/simpleWallet.sol
@@ -2,7 +2,7 @@ pragma solidity ^0.8.6;
 
 import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/access/Ownable.sol";
 
-contract SimpleWallet is Ownable {
+contract Allowance is Onwable {
     mapping(address => uint) public allowance;
 
     modifier ownerOrAllowed(uint _amount) {
@@ -14,7 +14,19 @@ contract SimpleWallet is Ownable {
         allowance[_who] = _amount;
     }
 
+    function reduceAllowance(address _who, uint _amount) internal {
+        allowance[_who] -= _amount;
+    }
+}
+
+contract SimpleWallet is Allowance {
     function withdraw(address payable _to, uint _amount) payable public ownerOrAllowed(_amount) {
+        require(_amount <= address(this).balance, "Nopt enought founds stored in this smart contract");
+        
+        if (!owner() == msg.sender) {
+            reduceAllowance(msg.sender, _amount);
+        }
+
         _to.transfer(_amount);
     }
 
