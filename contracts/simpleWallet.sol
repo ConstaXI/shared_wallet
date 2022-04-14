0bb7dbd61f44c414472100da28c7904dee366340
Davi Banfi <davibanfi1579@outlook.com>
2022-04-14 09:34:25 -0300

    fix: changed typo and sintax problemas

--- contracts/simpleWallet.sol
+++ contracts/simpleWallet.sol
@@ -2,10 +2,10 @@ pragma solidity ^0.8.6;
 
 import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/access/Ownable.sol";
 
-contract Allowance is Onwable {
+contract Allowance is Ownable {
     mapping(address => uint) public allowance;
 
-    event allowanceChanged(address indexed _forWho, address indexed _fromWhom, uint _newAmount);
+    event allowanceChanged(address indexed _forWho, address indexed _fromWhom, uint _oldAmount, uint _newAmount);
 
     modifier ownerOrAllowed(uint _amount) {
         require(owner() == msg.sender || allowance[msg.sender] >= _amount, "You are not allowed");
@@ -27,7 +27,7 @@ contract SimpleWallet is Allowance {
     function withdraw(address payable _to, uint _amount) payable public ownerOrAllowed(_amount) {
         require(_amount <= address(this).balance, "Nopt enought founds stored in this smart contract");
         
-        if (!owner() == msg.sender) {
+        if (!(owner() == msg.sender)) {
             reduceAllowance(msg.sender, _amount);
         }
 
