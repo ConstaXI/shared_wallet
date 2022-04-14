569554ad4155fa9995165d518674ccfddd0b6861
Davi Banfi <davibanfi1579@outlook.com>
2022-04-14 10:14:36 -0300

    refactor: detach allowance contract from simple wallet

--- contracts/simpleWallet.sol
+++ contracts/simpleWallet.sol
@@ -1,40 +1,30 @@
 pragma solidity ^0.8.6;
 
-import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/access/Ownable.sol";
-
-contract Allowance is Ownable {
-    mapping(address => uint) public allowance;
-
-    event allowanceChanged(address indexed _forWho, address indexed _fromWhom, uint _oldAmount, uint _newAmount);
-
-    modifier ownerOrAllowed(uint _amount) {
-        require(owner() == msg.sender || allowance[msg.sender] >= _amount, "You are not allowed");
-        _;
-    }
-
-    function addAllowance(address _who, uint _amount) public onlyOwner {
-        emit allowanceChanged(_who, msg.sender, allowance[_who], _amount);
-        allowance[_who] = _amount;
-    }
-
-    function reduceAllowance(address _who, uint _amount) internal {
-        emit allowanceChanged(_who, msg.sender, allowance[_who], allowance[_who] - _amount);
-        allowance[_who] -= _amount;
-    }
-}
+import "./alowance.sol";
 
 contract SimpleWallet is Allowance {
+    event MoneySent(address indexed _beneficiary, uint _amount);
+    event MoneyReceived(address indexed _from, uint _amount);
+
     function withdraw(address payable _to, uint _amount) payable public ownerOrAllowed(_amount) {
-        require(_amount <= address(this).balance, "Nopt enought founds stored in this smart contract");
+        require(_amount <= address(this).balance, "Not enought founds stored in this smart contract");
         
         if (!(owner() == msg.sender)) {
             reduceAllowance(msg.sender, _amount);
         }
 
+        emit MoneySent(_to, _amount);
+
         _to.transfer(_amount);
     }
 
-    fallback() external payable {}
+    function renounceOwnership() public override view onlyOwner {
+        revert("Can't renounce ownership.");
+    }
+
+    fallback() external payable {
+        emit MoneyReceived(msg.sender, msg.value);
+    }
 
     receive() external payable {}
 }
